a14585e27142aef44ea713bfe0e2c57017bcbca6
bnabi <bnabi@protonmail.com>
2023-01-05 13:12:10 +0530

    generate builder struct, initialize builder in function

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,14 +1,79 @@
 use proc_macro::TokenStream;
 
-use quote::quote;
-use::syn::{parse_macro_input, DeriveInput};
+use quote::{quote, format_ident, quote_spanned};
+use syn::{Data, Fields, Ident, FieldsNamed, parse_macro_input, DeriveInput};
+use syn::spanned::Spanned;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _input = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as DeriveInput);
     
+    let name = &input.ident;
+    let fields = parse_fields(&input.data);
+    
+    println!("fields:");
+    for field in fields.named.iter() {
+        if let Some(ref name) = field.ident {
+            println!("\t{}", name);
+        }
+    }
+
+    let builder_struct_name = format_ident!("{}Builder", name);
+    let builder_struct = get_builder_struct(&fields, &builder_struct_name);
+    let init_builder = init_builder_struct(&fields, &builder_struct_name);
+
     let expanded = quote!{
-        "Hello, world, from a derive macro"
+        #builder_struct
+        impl #name {
+            pub fn builder() -> #builder_struct_name {
+                #init_builder 
+            }
+        }
     };
+    eprintln!("TOKENS: {}", expanded);
     expanded.into()
 }
+
+fn parse_fields(data: &Data) -> FieldsNamed {
+    match *data {
+        Data::Struct(ref data) => {
+            match data.fields {
+                Fields::Named(ref fields) => {
+                    fields.clone()
+                },
+                _ => unimplemented!()
+            }
+        },
+        _ => unimplemented!()
+    }
+}
+
+fn get_builder_struct(fields: &FieldsNamed, name: &Ident) -> proc_macro2::TokenStream {
+    let recurse = fields.named.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        quote_spanned! { f.span()=>
+            #name: Option<#ty>,
+        }
+    });
+    quote! {
+        pub struct #name {
+            #(#recurse)*
+        }
+    }.into()
+}
+
+fn init_builder_struct(fields: &FieldsNamed, name: &Ident) -> proc_macro2::TokenStream {
+    let recurse = fields.named.iter().map(|f| {
+        let name = &f.ident;
+        quote_spanned! { f.span()=>
+            #name: None,
+        }
+    });
+    quote! {
+        #name {
+            #(#recurse)*
+        }
+    }.into()
+}
+
