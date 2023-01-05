e52964827228a2ba4a8e626c30e1198216aca533
bnabi <bnabi@protonmail.com>
2023-01-05 10:39:34 +0530

    proc macro basics, parse input

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,14 @@
 use proc_macro::TokenStream;
 
+use quote::quote;
+use::syn::{parse_macro_input, DeriveInput};
+
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let _input = parse_macro_input!(input as DeriveInput);
+    
+    let expanded = quote!{
+        "Hello, world, from a derive macro"
+    };
+    expanded.into()
 }
