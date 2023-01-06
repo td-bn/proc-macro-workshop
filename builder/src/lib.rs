b8e7223b92014b88065974d404fb915c06d27f47
bnabi <bnabi@protonmail.com>
2023-01-06 19:03:43 +0530

    add build method

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -13,7 +13,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let builder_struct_name = format_ident!("{}Builder", name);
     let builder_struct = get_builder_struct(&fields, &builder_struct_name);
-    let impl_builder = impl_builder(&fields, &builder_struct_name);
+    let impl_builder = impl_builder(&fields, &builder_struct_name, &name);
     let init_builder = init_builder_struct(&fields, &builder_struct_name);
 
     let expanded = quote! {
@@ -55,7 +55,7 @@ fn get_builder_struct(fields: &FieldsNamed, name: &Ident) -> proc_macro2::TokenS
     .into()
 }
 
-fn init_builder_struct(fields: &FieldsNamed, name: &Ident) -> proc_macro2::TokenStream {
+fn init_builder_struct(fields: &FieldsNamed, builder_name: &Ident) -> proc_macro2::TokenStream {
     let recurse = fields.named.iter().map(|f| {
         let name = &f.ident;
         quote_spanned! { f.span()=>
@@ -63,27 +63,47 @@ fn init_builder_struct(fields: &FieldsNamed, name: &Ident) -> proc_macro2::Token
         }
     });
     quote! {
-        #name {
+        #builder_name {
             #(#recurse)*
         }
     }
     .into()
 }
 
-fn impl_builder(fields: &FieldsNamed, name: &Ident) -> proc_macro2::TokenStream {
+fn impl_builder(fields: &FieldsNamed, builder_name: &Ident, struct_name: &Ident) -> proc_macro2::TokenStream {
     let recurse = fields.named.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
         quote_spanned! { f.span()=>
-            fn #name(&mut self, #name: #ty) -> &mut Self {
+            pub fn #name(&mut self, #name: #ty) -> &mut Self {
                 self.#name = Some(#name);
                 self
             }
         }
     });
+
+    let unwrap_build = fields.named.iter().map(|f| {
+        let name = &f.ident;
+        let name_string = name.clone().unwrap().to_string();
+        let error_message = format!("{} not present", name_string);
+        quote_spanned! { f.span()=>
+            #name: match &self.#name {
+                Some(v) => v.to_owned(),
+                None => return Err(#error_message.into())
+            },
+        }
+    });
+
     quote! {
-        impl #name {
+        impl #builder_name {
             #(#recurse)*
+
+            pub fn build(&mut self) -> Result<#struct_name, Box<dyn std::error::Error>> {
+                let s = #struct_name {
+                    #(#unwrap_build)*
+                };
+                Ok(s)
+            }
         }
     }
     .into()
